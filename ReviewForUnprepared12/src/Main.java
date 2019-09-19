10d744548367d929641759dca93e2a87e1af2b57
Paul Zaremba <pzaremba@vsb.bc.ca>
2019-09-19 11:09:45 -0700

    Starter Assignment

--- ReviewForUnprepared12/src/Main.java
+++ ReviewForUnprepared12/src/Main.java
@@ -1,21 +1,8 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        String word = "word";
-        Person aperson = new Person("Steve", 20);
-        System.out.println(aperson.name);
-        aperson.setAge(45);
-        System.out.println(aperson.getAge());
-        System.out.println(aperson);
-        ArrayList<Person> people = new ArrayList<>();
-        for(int i = 0; i < 10;i++){
-            people.add(new Person(Integer.toString(i),i));
+        MainMenu mainMenu = new MainMenu();
+        while(mainMenu.cont){
+            mainMenu.mainMenuStart();
         }
-        for(Person p : people){
-            System.out.println(p);
-        }
-        System.out.println(Person.idNumberCreator);
-
     }
 }
